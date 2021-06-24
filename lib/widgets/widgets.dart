32d19d81eacb61806e06019ab94885daa2c2b78c
felipelozano71@yahoo.com <mcfa9596>
2021-06-24 10:42:07 -0500

    Fin seccion 19 - Trazar ruta

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mapa_app/bloc/mapa/mapa_bloc.dart';
+
+
+import 'package:mapa_app/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
+
+part 'btn_ubicacion.dart';
+part 'btn_mi_ruta.dart';
+part 'btn_seguir_ubicacion.dart';
